0e329722f5a4a663a49676a68e959832df8854eb
Nano Miratus <nanomiratus@gmail.com>
2025-06-03 23:22:02 +0200

    format

--- sortatro.lua
+++ sortatro.lua
@@ -1,32 +1,32 @@
 SMODS.Atlas({
-    key = "modicon",
-    path = "sortatro.png",
-    px = 34,
-    py = 34
+	key = "modicon",
+	path = "sortatro.png",
+	px = 34,
+	py = 34
 });
 
 local oldSmodsCollectionPool = SMODS.collection_pool
 
 SMODS.collection_pool = function(basePool)
-    local pool = oldSmodsCollectionPool(basePool)
+	local pool = oldSmodsCollectionPool(basePool)
 
-    local alerts, others = {}, {}
-    for _, card in ipairs(pool) do
-        if card.discovered and not card.alerted then
-            table.insert(alerts, card)
-        else
-            table.insert(others, card)
-        end
-    end
-    for i = #pool, 1, -1 do
-        pool[i] = nil
-    end
-    for _, c in ipairs(alerts) do
-        table.insert(pool, c)
-    end
-    for _, c in ipairs(others) do
-        table.insert(pool, c)
-    end
+	local alerts, others = {}, {}
+	for _, card in ipairs(pool) do
+		if card.discovered and not card.alerted then
+			table.insert(alerts, card)
+		else
+			table.insert(others, card)
+		end
+	end
+	for i = #pool, 1, -1 do
+		pool[i] = nil
+	end
+	for _, c in ipairs(alerts) do
+		table.insert(pool, c)
+	end
+	for _, c in ipairs(others) do
+		table.insert(pool, c)
+	end
 
-    return pool
+	return pool
 end
